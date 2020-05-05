fc650ed8b56031b7334e80e7cbd0ce7b1c5cfe86
Vaibhav Pareek <vaibhavpareekvp7@gmail.com>
2020-05-05 09:06:53 +0530

    recursion

---
+++ LeetCode-Recursion/reversestring.cpp
@@ -0,0 +1,12 @@
+ // solution : using two pointer approach.
+class Solution {
+public:
+    void reverseString(vector<char>& s,int i=0) {
+       int n = s.size();
+        if(i==n/2)
+            return;
+        swap(s[i],s[n-i-1]);
+        reverseString(s,i+1);
+        
+    }
+};
